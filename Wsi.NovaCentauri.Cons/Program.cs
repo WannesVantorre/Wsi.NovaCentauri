b5a4fadcc41dcfcb005ca15c1273fc59b323a305
VantorreWannes <vantorrewannes@gmail.com>
2024-11-19 15:40:45 +0100

    feature: Implement code

--- Wsi.NovaCentauri.Cons/Program.cs
+++ Wsi.NovaCentauri.Cons/Program.cs
@@ -16,32 +16,19 @@ namespace Wsi.NovaCentauri.Cons
             string startUrl = "https://app-htf-2024.azurewebsites.net/api/a/medium/start";
             HttpResponseMessage startResponse = await client.GetAsync(startUrl);
             await Run(client);
-            //Test(client);
         }
 
         public static async Task Run(HackTheFutureClient client)
         {
-            string getUrl = "https://app-htf-2024.azurewebsites.net/api/a/medium/sample";
+            string getUrl = "https://app-htf-2024.azurewebsites.net/api/a/medium/puzzle";
             Game game = await client.GetFromJsonAsync<Game>(getUrl) ?? throw new InvalidDataException();
 
             BattleService battleService = new();
             GameResult gameResult = battleService.Run(game);
 
-            string postUrl = "https://app-htf-2024.azurewebsites.net/api/a/medium/sample";
+            string postUrl = "https://app-htf-2024.azurewebsites.net/api/a/medium/puzzle";
             HttpResponseMessage httpResponseMessage = await client.PostAsJsonAsync(postUrl, gameResult);
             Console.WriteLine(httpResponseMessage.StatusCode);
-        }
-
-        public static void Test(HackTheFutureClient client)
-        {
-            string input = "{\r\n  \"teamA\": [\r\n    {\r\n      \"strength\": 8,\r\n      \"speed\": 50,\r\n      \"health\": 96\r\n    },\r\n    {\r\n      \"strength\": 49,\r\n      \"speed\": 93,\r\n      \"health\": 114\r\n    },\r\n    {\r\n      \"strength\": 51,\r\n      \"speed\": 7,\r\n      \"health\": 63\r\n    },\r\n    {\r\n      \"strength\": 31,\r\n      \"speed\": 79,\r\n      \"health\": 149\r\n    },\r\n    {\r\n      \"strength\": 42,\r\n      \"speed\": 76,\r\n      \"health\": 89\r\n    }\r\n  ],\r\n  \"teamB\": [\r\n    {\r\n      \"strength\": 25,\r\n      \"speed\": 78,\r\n      \"health\": 113\r\n    },\r\n    {\r\n      \"strength\": 45,\r\n      \"speed\": 92,\r\n      \"health\": 104\r\n    },\r\n    {\r\n      \"strength\": 61,\r\n      \"speed\": 65,\r\n      \"health\": 56\r\n    },\r\n    {\r\n      \"strength\": 69,\r\n      \"speed\": 142,\r\n      \"health\": 15\r\n    },\r\n    {\r\n      \"strength\": 14,\r\n      \"speed\": 39,\r\n      \"health\": 68\r\n    }\r\n  ]\r\n}";
-            Game game = JsonSerializer.Deserialize<Game>(input) ?? throw new InvalidDataException();
-
-            BattleService battleService = new();
-            GameResult gameResult = battleService.Run(game);
-
-            string serialisedGameResult = JsonSerializer.Serialize(gameResult) ?? throw new InvalidDataException();
-            Console.WriteLine(serialisedGameResult);
         }
     }
 }
