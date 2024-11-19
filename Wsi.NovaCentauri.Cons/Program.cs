27abba4bbb4b57555123154c2285f5343fbf64ce
VantorreWannes <vantorrewannes@gmail.com>
2024-11-19 13:07:07 +0100

    Add project files.

---
+++ Wsi.NovaCentauri.Cons/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Wsi.NovaCentauri.Cons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
